1ccdb7f0ca87bd58ed2fbaf49e978ce400603a85
Abhishek D <abhidhaware1234@gmail.com>
2025-10-13 23:31:43 +0530

    MyProfile page : added HTML structure  and fix import in assets

--- MERN/AptDox/frontend/src/pages/MyProfile.jsx
+++ MERN/AptDox/frontend/src/pages/MyProfile.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { assets } from '../assets/assets_frontend/assets'
 
 const MyProfile = () => {
   return (
-    <div>MyProfile</div>
+    <div>
+      {/* images */}
+      <img src={assets.profile_pic} alt="" />
+      <img src={assets.upload_area} alt="" />
+    
+    {/* user name */}
+    <h1 >User Name</h1>
+    <hr />
+    <h2>contact Information</h2>
+    <p>Email: </p>
+    <p>Phone:</p>
+    <p>Address:</p>
+
+    <h2>Basic Information</h2>
+    <p>Gender:</p>
+    <p>Birthday:</p>
+
+    <button>Edit</button>
+    <button>Save information</button>
+    
+    </div>
   )
 }
 
