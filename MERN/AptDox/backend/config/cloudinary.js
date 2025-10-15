566758e18e3184f92f870692bd4af32bc54d1f94
Abhishek D <abhidhaware1234@gmail.com>
2025-10-15 22:00:07 +0530

    Backend: added cloudinary connect in config

--- MERN/AptDox/backend/config/cloudinary.js
+++ MERN/AptDox/backend/config/cloudinary.js
@@ -1,9 +1,24 @@
 import { v2 as cloudinary } from 'cloudinary'
 
-cloudinary.config({ 
-  cloud_name: 'AptDox', 
-  api_key: , 
-  api_secret: ,
+
+const cloudinaryConnect = async () => {
+  cloudinary.config({ 
+  cloud_name: process.env.CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_SECRET_KEY,
 //   secure_distribution: 'mydomain.com',
 //   upload_prefix: 'https://api-eu.cloudinary.com'
+    secure:true,
 });
+  
+}
+
+
+export default cloudinaryConnect;
+
+
+// import
+// config
+// CloudinaryStorage 
+// we dump storage into Multer
+// 
