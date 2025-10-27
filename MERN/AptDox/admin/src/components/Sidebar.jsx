4d74a8ffd505d5e7bc13ff2e55bce5f9be1c3050
Abhishek D <abhidhaware1234@gmail.com>
2025-10-27 18:23:04 +0530

    Sidebar : Added responsiveness

--- MERN/AptDox/admin/src/components/Sidebar.jsx
+++ MERN/AptDox/admin/src/components/Sidebar.jsx
@@ -12,60 +12,117 @@ const Sidebar = () => {
 
 
   return (
-    <div className='min-h-screen bg-white border-r'>
-      {
-        adminToken && <ul className='text-[#515151] mt-5'>
-          <NavLink className={({isActive})=>`flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ?'bg-[#F2F3FF] border-r-4 border-blue-500': ''}`} to={'/admin-dashboard'}>
-            <img src={assets.home_icon} alt="" />
-            <p className='block max-sm:hidden'>Dashboard</p>
-          </NavLink>
-          
-          <NavLink className={({isActive})=>`flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ?'bg-[#F2F3FF] border-r-4 border-blue-500': ''}`} to={'/all-appointments'}>
-            <img src={assets.appointment_icon} alt="" />
-            <p className='block max-sm:hidden'>Appointment</p>
-          </NavLink>
-
-          <NavLink className={({isActive})=>`flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ?'bg-[#F2F3FF] border-r-4 border-blue-500': ''}`} to={'/add-doctor'}>
-            <img src={assets.add_icon} alt="" />
-            <p className='block max-sm:hidden'>AddDoctor</p>
-          </NavLink>
-
-          <NavLink className={({isActive})=>`flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ?'bg-[#F2F3FF] border-r-4 border-blue-500': ''}`} to={'/doctor-list'}>
-            <img src={assets.people_icon} alt="" />
-            <p className='block max-sm:hidden'>DoctorList</p>
+    <div className="min-h-screen bg-white border-r w-20 sm:w-56 md:w-72 transition-all duration-300">
+      {/* ==============Admin sidebar================ */}
+      {adminToken && (
+        <ul className="text-[#515151] mt-5">
+          <NavLink
+            className={({ isActive }) =>
+              `flex items-center gap-3 py-3.5 px-3 sm:px-6 md:px-9 cursor-pointer transition-all duration-200 ${
+                isActive
+                  ? 'bg-[#F2F3FF] border-r-4 border-blue-500'
+                  : 'hover:bg-gray-50'
+              }`
+            }
+            to={'/admin-dashboard'}
+          >
+            <img className="w-5 sm:w-6" src={assets.home_icon} alt="" />
+            <p className="hidden sm:block">Dashboard</p>
           </NavLink>
 
-       
+          <NavLink
+            className={({ isActive }) =>
+              `flex items-center gap-3 py-3.5 px-3 sm:px-6 md:px-9 cursor-pointer transition-all duration-200 ${
+                isActive
+                  ? 'bg-[#F2F3FF] border-r-4 border-blue-500'
+                  : 'hover:bg-gray-50'
+              }`
+            }
+            to={'/all-appointments'}
+          >
+            <img className="w-5 sm:w-6" src={assets.appointment_icon} alt="" />
+            <p className="hidden sm:block">Appointment</p>
+          </NavLink>
 
+          <NavLink
+            className={({ isActive }) =>
+              `flex items-center gap-3 py-3.5 px-3 sm:px-6 md:px-9 cursor-pointer transition-all duration-200 ${
+                isActive
+                  ? 'bg-[#F2F3FF] border-r-4 border-blue-500'
+                  : 'hover:bg-gray-50'
+              }`
+            }
+            to={'/add-doctor'}
+          >
+            <img className="w-5 sm:w-6" src={assets.add_icon} alt="" />
+            <p className="hidden sm:block">AddDoctor</p>
+          </NavLink>
 
-
+          <NavLink
+            className={({ isActive }) =>
+              `flex items-center gap-3 py-3.5 px-3 sm:px-6 md:px-9 cursor-pointer transition-all duration-200 ${
+                isActive
+                  ? 'bg-[#F2F3FF] border-r-4 border-blue-500'
+                  : 'hover:bg-gray-50'
+              }`
+            }
+            to={'/doctor-list'}
+          >
+            <img className="w-5 sm:w-6" src={assets.people_icon} alt="" />
+            <p className="hidden sm:block">DoctorList</p>
+          </NavLink>
         </ul>
-      }
+      )}
 
       {/* ==============Doctor sidebar================  */}
-
-      {
-        doctorToken && <ul className='text-[#515151] mt-5'>
-          <NavLink className={({isActive})=>`flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ?'bg-[#F2F3FF] border-r-4 border-blue-500': ''}`} to={'/doctor-dashboard'}>
-            <img src={assets.home_icon} alt="" />
-            <p className='block max-sm:hidden'>Dashboard</p>
-          </NavLink>
-          
-          <NavLink className={({isActive})=>`flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ?'bg-[#F2F3FF] border-r-4 border-blue-500': ''}`} to={'/doctor-appointments'}>
-            <img src={assets.appointment_icon} alt="" />
-            <p className='block max-sm:hidden'>Appointments</p>
-          </NavLink>
-
-          <NavLink className={({isActive})=>`flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ?'bg-[#F2F3FF] border-r-4 border-blue-500': ''}`} to={'/doctor-profile'}>
-            <img src={assets.people_icon} alt="" />
-            <p className='block max-sm:hidden'>Profile</p>
+      {doctorToken && (
+        <ul className="text-[#515151] mt-5">
+          <NavLink
+            className={({ isActive }) =>
+              `flex items-center gap-3 py-3.5 px-3 sm:px-6 md:px-9 cursor-pointer transition-all duration-200 ${
+                isActive
+                  ? 'bg-[#F2F3FF] border-r-4 border-blue-500'
+                  : 'hover:bg-gray-50'
+              }`
+            }
+            to={'/doctor-dashboard'}
+          >
+            <img className="w-5 sm:w-6" src={assets.home_icon} alt="" />
+            <p className="hidden sm:block">Dashboard</p>
           </NavLink>
 
+          <NavLink
+            className={({ isActive }) =>
+              `flex items-center gap-3 py-3.5 px-3 sm:px-6 md:px-9 cursor-pointer transition-all duration-200 ${
+                isActive
+                  ? 'bg-[#F2F3FF] border-r-4 border-blue-500'
+                  : 'hover:bg-gray-50'
+              }`
+            }
+            to={'/doctor-appointments'}
+          >
+            <img className="w-5 sm:w-6" src={assets.appointment_icon} alt="" />
+            <p className="hidden sm:block">Appointments</p>
+          </NavLink>
+
+          <NavLink
+            className={({ isActive }) =>
+              `flex items-center gap-3 py-3.5 px-3 sm:px-6 md:px-9 cursor-pointer transition-all duration-200 ${
+                isActive
+                  ? 'bg-[#F2F3FF] border-r-4 border-blue-500'
+                  : 'hover:bg-gray-50'
+              }`
+            }
+            to={'/doctor-profile'}
+          >
+            <img className="w-5 sm:w-6" src={assets.people_icon} alt="" />
+            <p className="hidden sm:block">Profile</p>
+          </NavLink>
         </ul>
-      }
-
+      )}
     </div>
   )
+
 }
 
 export default Sidebar
